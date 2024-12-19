be93cf1df431796439ebfa2fba3a13ebec35094d
Adinug122 <adi315429@gmail.com>
2024-12-19 09:26:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Toko Sayur syaiful';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Selamat Datang</h1>
+
+        <p class="lead">Anda datang ke tempat yang tepat</p>
+
+        <p><a class="btn btn-lg btn-success" href="/index.php?r=tbl-barang">Klik disini untuk melihat daftar barang</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4 mb-3">
+                <h2>Cabai Rawit</h2>
+                <img src="/images/CabaiRawit.jpg" width="300">
+
+                <p>"Rasakan sensasi pedas cabai rawit yang menggugah selera! Cocok untuk sambal, bumbu dapur, dan kreasi kuliner lainnya."</p>
+
+                
+            </div>
+             
+            <div class="col-lg-4 mb-3">
+                <h2>Tomat</h2>
+                <img src="/images/Tomat.jpg" width="300">
+                <p>"Tomat segar berkualitas tinggi untuk kebutuhan dapur Anda! Rasanya yang alami dan nutrisinya yang melimpah akan menjadikan masakan Anda lebih istimewa."</p>
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Bawang putih</h2>
+                    <img src="/images/bawang.jpg" width="300">
+
+                <p>"Dapatkan bawang putih segar berkualitas tinggi untuk kelezatan masakan Anda! Aroma khas dan manfaatnya yang melimpah akan membuat setiap hidangan semakin spesial."</p>
+
+              
+            </div>
+        </div>
+
+    </div>
+</div>
